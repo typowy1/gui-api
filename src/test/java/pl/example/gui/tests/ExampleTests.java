62f63bb3467fcca74de7f8710f530ec6d4f80a28
Rafał Leśnicki <Rafal.Lesnicki@onwelo.com>
2023-11-17 16:32:44 +0100

    fix

--- src/test/java/pl/example/gui/tests/ExampleTests.java
+++ src/test/java/pl/example/gui/tests/ExampleTests.java
@@ -14,30 +14,30 @@ public class ExampleTests extends BaseTest {
     AboutUsPage aboutUsPage;
     FooterPage footerPage;
 
-    @Issue("Bug 1") //id defektu, przekieruje do defektu
-    @TmsLink("ID333") // id przypadku testowego, przekieruje do testu
-    @Severity(SeverityLevel.BLOCKER)
-    @Test()
-    @Description("Check if the About As tab will return the correct page")
-    public void isAboutUsLinkClickable() {
-        topMenuPage = new TopMenuPage();
-        topMenuPage.clickOnAboutUsLink();
-
-        aboutUsPage = new AboutUsPage();
-        assertThat(aboutUsPage.getTextFromHeadingH1()).isEqualTo("WE DELIVER\n" + "YOUR IDEAS");
-    }
-
-//    @Issue("Bug 2") //id defektu, przekieruje do defektu
-//    @TmsLink("ID444") // id przypadku testowego, przekieruje do testu
+//    @Issue("Bug 1") //id defektu, przekieruje do defektu
+//    @TmsLink("ID333") // id przypadku testowego, przekieruje do testu
 //    @Severity(SeverityLevel.BLOCKER)
 //    @Test()
 //    @Description("Check if the About As tab will return the correct page")
-//    public void isAboutUsLinkClickableFailedTest() {
+//    public void isAboutUsLinkClickable() {
 //        topMenuPage = new TopMenuPage();
 //        topMenuPage.clickOnAboutUsLink();
+//
 //        aboutUsPage = new AboutUsPage();
-//        assertThat(aboutUsPage.getTextFromHeadingH1()).isEqualTo("WE DELIVER");
+//        assertThat(aboutUsPage.getTextFromHeadingH1()).isEqualTo("WE DELIVER\n" + "YOUR IDEAS");
 //    }
+
+    @Issue("Bug 2") //id defektu, przekieruje do defektu
+    @TmsLink("ID444") // id przypadku testowego, przekieruje do testu
+    @Severity(SeverityLevel.BLOCKER)
+    @Test()
+    @Description("Check if the About As tab will return the correct page")
+    public void isAboutUsLinkClickableFailedTest() {
+        topMenuPage = new TopMenuPage();
+        topMenuPage.clickOnAboutUsLink();
+        aboutUsPage = new AboutUsPage();
+        assertThat(aboutUsPage.getTextFromHeadingH1()).isEqualTo("WE DELIVER");
+    }
 
     @Issue("Bug 3") //id defektu, przekieruje do defektu
     @TmsLink("ID567") // id przypadku testowego, przekieruje do testu
@@ -51,7 +51,7 @@ public class ExampleTests extends BaseTest {
                         "ul. Prosta 67\n" +
                         "00-838 Warszawa");
     }
-
+//
 //    @Issue("Bug 4") //id defektu, przekieruje do defektu
 //    @TmsLink("ID5655") // id przypadku testowego, przekieruje do testu
 //    @Severity(SeverityLevel.MINOR)
